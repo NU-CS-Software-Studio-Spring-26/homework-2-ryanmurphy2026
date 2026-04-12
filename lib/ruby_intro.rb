ad606ce1f35abe515f9bff0deffe1a5d4dd738bf
ryanmurphy2026 <ryanmurphy2026@u.northwestern.edu>
2026-04-12 16:37:44 -0500

    updated code

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,50 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  arr.sum(0)
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  (arr.max(2)).sum(0)
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  arr.any? do|a|
+    arr.any? do |b|
+      a != b && a + b == n
+    end
+  end
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  !(s.start_with?("A", "E", "I", "O", "U", "a", "e", "i", "o", "u"))
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  str.chars.all? { |x| x == 0 || x == 1}
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize(isbn, price)
+    raise ArgumentError, "ISBN must be a string" unless isbn.is_a?(String)
+    raise ArgumentError, "ISBN must have value" unless !(isbn.empty?(String))
+    raise ArgumentError, "price must be a floating point" unless age.is_a?(Float)
+    raise ArgumentError, "price must be a floating point" unless age.is_a?(Float)
+
+    @isbn = isbn
+    @price = price
+  end
+
+
+
+
+
 end
